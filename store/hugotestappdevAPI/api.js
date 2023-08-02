4b0ef3a7520f333c95d7d52962558b27a7b3e68d
Crowdbotics <engineering@crowdbotics.com>
2023-08-02 14:38:52 +0000

    Update store.

--- store/hugotestappdevAPI/api.js
+++ store/hugotestappdevAPI/api.js
@@ -1,170 +1,115 @@
-import axios from "axios";
+import axios from "axios"
 const hugotestappdevAPI = axios.create({
   baseURL: "https://hugo-testapp-dev-105851.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return hugotestappdevAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return hugotestappdevAPI.post(`/api/v1/login/`, payload);
+  return hugotestappdevAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return hugotestappdevAPI.post(`/api/v1/signup/`, payload);
+  return hugotestappdevAPI.post(`/api/v1/signup/`, payload)
 }
-
-function modules_articles_article_list(payload) {
-  return hugotestappdevAPI.get(`/modules/articles/article/`);
-}
-
-function modules_articles_article_create(payload) {
-  return hugotestappdevAPI.post(`/modules/articles/article/`, payload);
-}
-
-function modules_articles_article_retrieve(payload) {
-  return hugotestappdevAPI.get(`/modules/articles/article/${payload.id}/`);
-}
-
-function modules_articles_article_update(payload) {
-  return hugotestappdevAPI.put(`/modules/articles/article/${payload.id}/`, payload);
-}
-
-function modules_articles_article_partial_update(payload) {
-  return hugotestappdevAPI.patch(`/modules/articles/article/${payload.id}/`, payload);
-}
-
-function modules_articles_article_destroy(payload) {
-  return hugotestappdevAPI.delete(`/modules/articles/article/${payload.id}/`);
-}
-
 function modules_hubspot_service_access_token_create(payload) {
-  return hugotestappdevAPI.post(`/modules/hubspot/service/access/token/`);
+  return hugotestappdevAPI.post(`/modules/hubspot/service/access/token/`)
 }
-
 function modules_hubspot_service_contact_deals_list_retrieve(payload) {
-  return hugotestappdevAPI.get(`/modules/hubspot/service/contact/deals/list/`);
+  return hugotestappdevAPI.get(`/modules/hubspot/service/contact/deals/list/`)
 }
-
 function modules_hubspot_service_deals_associations_create_create(payload) {
-  return hugotestappdevAPI.post(`/modules/hubspot/service/deals/associations/create/`);
+  return hugotestappdevAPI.post(
+    `/modules/hubspot/service/deals/associations/create/`
+  )
 }
-
 function modules_hubspot_service_deals_create_create(payload) {
-  return hugotestappdevAPI.post(`/modules/hubspot/service/deals/create/`);
+  return hugotestappdevAPI.post(`/modules/hubspot/service/deals/create/`)
 }
-
 function modules_hubspot_service_deals_list_retrieve(payload) {
-  return hugotestappdevAPI.get(`/modules/hubspot/service/deals/list/`);
+  return hugotestappdevAPI.get(`/modules/hubspot/service/deals/list/`)
 }
-
 function modules_hubspot_service_deals_remove_destroy(payload) {
-  return hugotestappdevAPI.delete(`/modules/hubspot/service/deals/remove/`);
+  return hugotestappdevAPI.delete(`/modules/hubspot/service/deals/remove/`)
 }
-
 function modules_hubspot_service_deals_single_retrieve(payload) {
-  return hugotestappdevAPI.get(`/modules/hubspot/service/deals/single/`);
+  return hugotestappdevAPI.get(`/modules/hubspot/service/deals/single/`)
 }
-
 function modules_hubspot_service_events_create_create(payload) {
-  return hugotestappdevAPI.post(`/modules/hubspot/service/events/create/`);
+  return hugotestappdevAPI.post(`/modules/hubspot/service/events/create/`)
 }
-
 function modules_hubspot_service_meeting_contacts_list_retrieve(payload) {
-  return hugotestappdevAPI.get(`/modules/hubspot/service/meeting/contacts/list/`);
+  return hugotestappdevAPI.get(
+    `/modules/hubspot/service/meeting/contacts/list/`
+  )
 }
-
 function modules_hubspot_service_ticket_associations_create_update(payload) {
-  return hugotestappdevAPI.put(`/modules/hubspot/service/ticket/associations/create/`);
+  return hugotestappdevAPI.put(
+    `/modules/hubspot/service/ticket/associations/create/`
+  )
 }
-
 function modules_hubspot_service_ticket_associations_list_retrieve(payload) {
-  return hugotestappdevAPI.get(`/modules/hubspot/service/ticket/associations/list/`);
+  return hugotestappdevAPI.get(
+    `/modules/hubspot/service/ticket/associations/list/`
+  )
 }
-
 function modules_hubspot_service_tickets_create_create(payload) {
-  return hugotestappdevAPI.post(`/modules/hubspot/service/tickets/create/`);
+  return hugotestappdevAPI.post(`/modules/hubspot/service/tickets/create/`)
 }
-
 function modules_hubspot_service_tickets_list_retrieve(payload) {
-  return hugotestappdevAPI.get(`/modules/hubspot/service/tickets/list/`);
+  return hugotestappdevAPI.get(`/modules/hubspot/service/tickets/list/`)
 }
-
 function modules_hubspot_service_tickets_remove_destroy(payload) {
-  return hugotestappdevAPI.delete(`/modules/hubspot/service/tickets/remove/`);
+  return hugotestappdevAPI.delete(`/modules/hubspot/service/tickets/remove/`)
 }
-
 function modules_hubspot_service_tickets_single_retrieve(payload) {
-  return hugotestappdevAPI.get(`/modules/hubspot/service/tickets/single/`);
+  return hugotestappdevAPI.get(`/modules/hubspot/service/tickets/single/`)
 }
-
 function modules_hubspot_service_webhook_create(payload) {
-  return hugotestappdevAPI.post(`/modules/hubspot/service/webhook/`);
+  return hugotestappdevAPI.post(`/modules/hubspot/service/webhook/`)
 }
-
 function rest_auth_login_create(payload) {
-  return hugotestappdevAPI.post(`/rest-auth/login/`, payload);
+  return hugotestappdevAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return hugotestappdevAPI.get(`/rest-auth/logout/`);
+  return hugotestappdevAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return hugotestappdevAPI.post(`/rest-auth/logout/`);
+  return hugotestappdevAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return hugotestappdevAPI.post(`/rest-auth/password/change/`, payload);
+  return hugotestappdevAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return hugotestappdevAPI.post(`/rest-auth/password/reset/`, payload);
+  return hugotestappdevAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return hugotestappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return hugotestappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return hugotestappdevAPI.post(`/rest-auth/registration/`, payload);
+  return hugotestappdevAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return hugotestappdevAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return hugotestappdevAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return hugotestappdevAPI.get(`/rest-auth/user/`);
+  return hugotestappdevAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return hugotestappdevAPI.put(`/rest-auth/user/`, payload);
+  return hugotestappdevAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return hugotestappdevAPI.patch(`/rest-auth/user/`, payload);
+  return hugotestappdevAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
-  modules_articles_article_list,
-  modules_articles_article_create,
-  modules_articles_article_retrieve,
-  modules_articles_article_update,
-  modules_articles_article_partial_update,
-  modules_articles_article_destroy,
   modules_hubspot_service_access_token_create,
   modules_hubspot_service_contact_deals_list_retrieve,
   modules_hubspot_service_deals_associations_create_create,
@@ -192,4 +137,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
